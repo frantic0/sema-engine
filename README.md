031e99710b98090d46600458e7fc3a2f31767a4c
Francisco Bernardo <f.bernardo@sussex.ac.uk>
2021-01-10 19:03:31 +0000

    rollup is not cutting it for wasm

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # sema-engine
+
+`sema-engine` is uses an Emscripten port of the Maximilian library to WebAssembly (Wasm) and to ES module
+# Building
+
+The `dist/` folder will contain the required files, tests, and examples after building.
+
+### Download Maximilian library:
+```
+$ git submodule update --init
+```
--- rollup.config.js
+++ rollup.config.js
@@ -1,48 +1,49 @@
-import resolve from "@rollup/plugin-node-resolve";
-import { terser } from "rollup-plugin-terser";
-import commonjs from "@rollup/plugin-commonjs";
-import pkg from "./package.json";
-import eslint from "@rollup/plugin-eslint";
-import wasm from "@rollup/plugin-wasm";
+import resolve from '@rollup/plugin-node-resolve';
+import { terser } from 'rollup-plugin-terser';
+import commonjs from '@rollup/plugin-commonjs';
+import pkg from './package.json';
+import eslint from '@rollup/plugin-eslint';
+import wasm from '@rollup/plugin-wasm';
 
 
 export default [
-	// browser-friendly UMD build
-	{
-		input: "src/index.js",
-		output: [
-			{ file: pkg.module, format: "es" },
-			{ file: pkg.main, format: "umd", name: "sema-engine" },
-		],
-		plugins: [
-			resolve(), // so Rollup can find `nearley`
-			commonjs(), // so Rollup can convert `nearley` to an ES module
-			terser(),
-			// wasm({
-			// 	sync: [
-			// 		"src/engine/maximilian.wasmmodule.js",
-			// 		"src/engine/open303.wasmmodule.js",
-			// 	],
-			// }),
-			// eslint({
-			// 	/* your options */
-			// }),
-		],
-	},
+  // browser-friendly UMD build
+  {
+    input: 'src/index.js',
+    output: [
+      { file: pkg.module, format: 'es' },
+      { file: pkg.main, format: 'umd', name: 'sema-engine' },
+    ],
+    plugins: [
+      resolve(), // so Rollup can find `nearley`
+      commonjs(), // so Rollup can convert `nearley` to an ES module
+      terser(),
+      wasm()
+      // wasm({
+      // 	sync: [
+      // 		"src/engine/maximilian.wasmmodule.js",
+      // 		"src/engine/open303.wasmmodule.js",
+      // 	],
+      // }),
+      // eslint({
+      // 	/* your options */
+      // }),
+    ],
+  },
 
-	// CommonJS (for Node) and ES module (for bundlers) build.
-	// (We could have three entries in the configuration array
-	// instead of two, but it's quicker to generate multiple
-	// builds from a single configuration where possible, using
-	// an array for the `output` option, where we can specify
-	// `file` and `format` for each target)
-	{
-		input: "src/index.js",
-		external: ["nearley"],
-		output: [
-			{ file: pkg.main, format: "cjs" },
-			{ file: pkg.module, format: "es" },
-		],
-	},
+  // CommonJS (for Node) and ES module (for bundlers) build.
+  // (We could have three entries in the configuration array
+  // instead of two, but it's quicker to generate multiple
+  // builds from a single configuration where possible, using
+  // an array for the `output` option, where we can specify
+  // `file` and `format` for each target)
+  {
+    input: 'src/index.js',
+    external: ['nearley'],
+    output: [
+      { file: pkg.main, format: 'cjs' },
+      { file: pkg.module, format: 'es' },
+    ],
+  },
 ];
 
