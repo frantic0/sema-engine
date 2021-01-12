251251479ba67cd59e62c1cb3be197c1d01b33bb
Francisco Bernardo <f.bernardo@sussex.ac.uk>
2021-01-12 00:50:28 +0000

    compiling to examples

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 export { AudioEngine } from './engine/audioEngine.js';
-import Maximilian from 'maximilian-wasm/maximilian.wasm';
+// import Maximilian from 'maximilian-wasm/maximilian.wasm';
 
-console.log(Maximilian);
+// console.log(Maximilian);
 // This fails because it is not importing Nearley node module
 // export { compile } from '../src/compiler/compiler.js';
--- webpack.config.js
+++ webpack.config.js
@@ -2,18 +2,72 @@ const path = require("path");
 
 module.exports = {
 	entry: "./src/index.js",
+	mode: "development",
 	output: {
 		path: path.resolve(__dirname, "dist"),
 		filename: "sema-engine.js",
-		library: "sema-engine",
+		library: "sema-engine", //  make library available for consumption in different environments
 		libraryTarget: "umd",
-	},
-	externals: {
-		nearley: {
-			commonjs: "nearley",
-			commonjs2: "nearley",
-			amd: "nearley",
-			root: "_",
+		auxiliaryComment: {
+			root: "Root Comment",
+			commonjs: "CommonJS Comment",
+			commonjs2: "CommonJS2 Comment",
+			amd: "AMD Comment",
 		},
+	},
+	// externals: {
+	// 	nearley: {
+	// 		commonjs: "nearley",
+	// 		commonjs2: "nearley",
+	// 		amd: "nearley",
+	// 		root: "nearley",
+	// 	},
+	// },
+	module: {
+		rules: [
+			{
+				test: /\.worker\.js/,
+				use: {
+					loader: "worker-loader",
+					options: { fallback: true },
+				},
+			},
+			{
+				// test: /\.(jpg|png|svg)$/,
+				test: /\.wasmmodule.js$/,
+				loader: "url-loader",
+				options: {
+					limit: Infinity, // everything
+				},
+			},
+			{
+				test: /maxi-processor.js/,
+				loader: "file-loader", // files should NOT get processed, only emitted
+				options: {
+					name: "maxi-processor.js",
+				},
+			},
+			{
+				test: /ringbuf.js/,
+				loader: "file-loader", // files should NOT get processed, only emitted
+				options: {
+					name: "ringbuf.js",
+				},
+			},
+			{
+				test: /\.wasm$/,
+				type:
+					"javascript/auto" /** this disables webpacks default handling of wasm */,
+				use: [
+					{
+						loader: "file-loader",
+						options: {
+							name: "wasm/[name].[hash].[ext]",
+							publicPath: "/dist/",
+						},
+					},
+				],
+			},
+		],
 	},
 };
