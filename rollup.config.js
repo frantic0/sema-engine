888e6c8a5656c6809c7f82ceaf6b3323821b92dc
Francisco Bernardo <f.bernardo@sussex.ac.uk>
2021-01-14 19:53:35 +0000

    conditional build for dev/production based on environment variable for rollup

--- rollup.config.js
+++ rollup.config.js
@@ -8,7 +8,7 @@ import livereload from "rollup-plugin-livereload";
 
 // import eslint from "@rollup/plugin-eslint";
 
-const mode = process.env.BUILD || "development";
+const isDevelopment = process.env.BUILD || "development";
 
 export default [
 	// browser-friendly UMD build
@@ -18,77 +18,109 @@ export default [
 			{
 				file: pkg.module,
 				format: "es",
-				sourcemap: mode === "development" ? true : "inline",
+				sourcemap: isDevelopment ? true : "inline",
 			},
 			{
 				file: pkg.main,
 				format: "umd",
 				name: "sema-engine",
-				sourcemap: mode === "development" ? true : "inline",
+				sourcemap: isDevelopment ? true : "inline",
 			},
 			{
 				file: "dist/sema-engine.min.js",
 				format: "iife",
 				name: "version",
-				sourcemap: mode === "development" ? true : "inline",
+				sourcemap: isDevelopment ? true : "inline",
 				plugins: [terser()],
 			},
 		],
 		plugins: [
-			resolve(), // so Rollup can find `nearley`
-			commonjs(), // so Rollup can convert `nearley` to an ES module
-			// {
-			// 	// debug the Rollup bundling process by injecting a hook in the plugin chain!
-			// 	transform(code, id) {
-			// 		console.log(id);
-			// 		console.log(code);
-			// 		// not returning anything, so doesn't affect bundle
-			// 	},
-			// },
-			terser(),
-			copy({
-				targets: [
-					{
-						src: "src/engine/maxi-processor.js",
-						dest: ["dist", "example"],
-					},
-					{
-						src: "src/engine/maximilian.wasmmodule.js",
-						dest: ["dist", "example"],
-					},
-					{
-						src: "src/engine/open303.wasmmodule.js",
-						dest: ["dist", "example"],
-					},
-					{
-						// ringbuf is imported by both the Engine (AW node) and maxi-processor (AWP) so needs to be both bundled AND copied!
-						src: "src/engine/ringbuf.js",
-						dest: ["dist", "example"],
-					},
-					{
-						src: "assets/*",
-						dest: "dist",
-					},
-				],
-			}),
-			serve({
-				open: true, // Launch in browser (default: false)
-				verbose: true, // Show server address in console (default: true)
-				contentBase: "dist", // Folder to serve files from
-				historyApiFallback: false, // Set to true to return index.html (200) instead of error page (404)
-				historyApiFallback: "/200.html", // Path to fallback page
-				host: "localhost", // Options used in setting up server
-				port: 9002, // Use this port in you VSCode
-				mimeTypes: {
-					// set custom mime types, usage https://github.com/broofa/mime#mimedefinetypemap-force--false
-					"application/javascript": ["js"],
-					"application/wasm": ["wasm"]
-				},
-			}),
-			livereload({
-				watch: "dist",
-				verbose: true,
-			}),
+			...(isDevelopment
+				? [
+						resolve(), // so Rollup can find `nearley`
+						commonjs(), // so Rollup can convert `nearley` to an ES module
+						// {
+						// 	transform(code, id) {
+						// 		// debug the Rollup bundling process by injecting a hook in the plugin chain!
+						// 		console.log(id);
+						// 		console.log(code);
+						// 	}, // not returning anything, so doesn't affect bundle
+						// },
+						terser(),
+						copy({
+							targets: [
+								{
+									src: "src/engine/maxi-processor.js",
+									dest: ["dist", "example"],
+								},
+								{
+									src: "src/engine/maximilian.wasmmodule.js",
+									dest: ["dist", "example"],
+								},
+								{
+									src: "src/engine/open303.wasmmodule.js",
+									dest: ["dist", "example"],
+								},
+								{
+									// ringbuf is imported by both the Engine (AW node) and maxi-processor (AWP) so needs to be both bundled AND copied!
+									src: "src/engine/ringbuf.js",
+									dest: ["dist", "example"],
+								},
+								{
+									src: "assets/*",
+									dest: "dist",
+								},
+							],
+						}),
+						serve({
+							open: true, // Launch in browser (default: false)
+							verbose: true, // Show server address in console (default: true)
+							contentBase: "dist", // Folder to serve files from
+							historyApiFallback: false, // Set to true to return index.html (200) instead of error page (404)
+							historyApiFallback: "/200.html", // Path to fallback page
+							host: "localhost", // Options used in setting up server
+							port: 9002, // Use this port in you VSCode
+							mimeTypes: {
+								// set custom mime types, usage https://github.com/broofa/mime#mimedefinetypemap-force--false
+								"application/javascript": ["js"],
+								"application/wasm": ["wasm"],
+							},
+						}),
+						livereload({
+							watch: "dist",
+							verbose: true,
+						}),
+				  ]
+				: [
+						resolve(), // so Rollup can find `nearley`
+						commonjs(), // so Rollup can convert `nearley` to an ES module
+						terser(),
+						copy({
+							targets: [
+								{
+									src: "src/engine/maxi-processor.js",
+									dest: ["dist", "example"],
+								},
+								{
+									src: "src/engine/maximilian.wasmmodule.js",
+									dest: ["dist", "example"],
+								},
+								{
+									src: "src/engine/open303.wasmmodule.js",
+									dest: ["dist", "example"],
+								},
+								{
+									// ringbuf is imported by both the Engine (AW node) and maxi-processor (AWP) so needs to be both bundled AND copied!
+									src: "src/engine/ringbuf.js",
+									dest: ["dist", "example"],
+								},
+								{
+									src: "assets/open303.wasmmodule.js",
+									dest: "dist",
+								},
+							],
+						}),
+				  ]),
 			// eslint({
 			// 	/* your options */
 			// }),
