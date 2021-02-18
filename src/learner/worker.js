f8b45194965e285d38404ee1ce32f2fda20b9fec
Francisco Bernardo <f.bernardo@sussex.ac.uk>
2021-02-18 04:41:18 +0000

    removed unused deps; improved error handling on learner worker importscripts; bumped patch version

--- src/learner/worker.js
+++ src/learner/worker.js
@@ -3,170 +3,160 @@
   function gevalAll() {
 		if (!geval) {
 			var geval = eval; // puts eval into global scope https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/eval
-			geval("var input = (value, channel) => {}");
-			geval(
-				"var output = (value,channel) => {postMessage({func:'data', val:value, ch:channel});}"
-			);
-
-			geval(`
-var outputSABs = {};
-class MLSABOutputTransducer {
-  constructor(bufferType, channel, blocksize) {
-    this.channel = channel;
-    this.blocksize = blocksize;
-
-    //check for existing channels
-    if (channel in outputSABs && outputSABs[channel].blocksize == blocksize) {
-      //reuse existing
-      this.ringbuf = outputSABs[channel].rb;
-    }else{
-      //create a new SAB and notify the receiver
-      this.sab = RingBuffer.getStorageForCapacity(32 * blocksize, Float64Array);
-      this.ringbuf = new RingBuffer(this.sab, Float64Array);
-      outputSABs[channel] = {rb:this.ringbuf, sab:this.sab, created:Date.now(), blocksize:blocksize};
-
-      postMessage({
-        func: 'sab',
-        value: this.sab,
-        ttype: bufferType,
-        channelID: channel,
-        blocksize:blocksize
-      });
     }
-  }
+	  try {
+ 			geval("var input = (value, channel) => {}");
+ 			geval(
+ 				"var output = (value,channel) => { postMessage({func:'data', val:value, ch:channel});}"
+ 			);
+ 			geval(`
+         var outputSABs = {};
+         class MLSABOutputTransducer {
+           constructor(bufferType, channel, blocksize) {
+             this.channel = channel;
+             this.blocksize = blocksize;
+             //check for existing channels
+             if (channel in outputSABs && outputSABs[channel].blocksize == blocksize) {
+               //reuse existing
+               this.ringbuf = outputSABs[channel].rb;
+             }else{
+               //create a new SAB and notify the receiver
+               this.sab = RingBuffer.getStorageForCapacity(32 * blocksize, Float64Array);
+               this.ringbuf = new RingBuffer(this.sab, Float64Array);
+               outputSABs[channel] = {rb:this.ringbuf, sab:this.sab, created:Date.now(), blocksize:blocksize};
+               postMessage({
+                 func: 'sab',
+                 value: this.sab,
+                 ttype: bufferType,
+                 channelID: channel,
+                 blocksize:blocksize
+               });
+             }
+           }
+           send(value) {
+             if (this.ringbuf.available_write() > 1) {
+               if (typeof(value) == "number") {
+                 this.ringbuf.push(new Float64Array([value]));
+               }else{
+                 if (value.length == this.blocksize) {
+                   this.ringbuf.push(value);
+                 }else if (value.length < this.blocksize) {
+                   let newVal = new Float64Array(this.blocksize);
+                   for(let i in value) newVal[i] = value[i];
+                   this.ringbuf.push(newVal);
+                 }else{
+                   this.ringbuf.push(value.slice(0,this.blocksize));
+                 }
+               }
+             }
+           }
+         }
+         var createOutputChannel = (id, blocksize) => {
+           return new MLSABOutputTransducer('ML', id, blocksize);
+         };
+       `);
+  		geval(`
+          var loadResponders = {};
+          var inputSABs={};
+          var sema = {
+            saveF32Array: (name, val) => {
+              postMessage({
+                "func": "save",
+                "name": name,
+                "val": val
+              });
+              return 0;
+            },
+            loadF32Array: (name, onload) => {
+              postMessage({
+                "func": "load",
+                "name": name,
+              });
+              loadResponders[name] = onload;
+              return 0;
+            },
+            download: (name) => {
+              postMessage({
+                "func": "download",
+                "name": name,
+              });
+            },
+            sendCode: (code) => {
+              postMessage({
+                "func": "sendcode",
+                "code": code,
+              });
+            },
+            pbcopy: (msg) => {
+              postMessage({
+                "func": "pbcopy",
+                "msg": msg,
+              });
+            },
+            sendBuffer: (bufferName,data) => {
+                postMessage({
+                    "func": "sendbuf",
+                    "name": bufferName,
+                    "data": data
+                });
+            },
+            env: {
+              saveLocal: (name) => {
+                postMessage({
+                      "func": "envsave",
+                      "name": name,
+                      "storage":"local"
+                  }
+                )
+              },
+              loadLocal: (name) => {
+                postMessage({
+                      "func": "envload",
+                      "name": name,
+                      "storage":"local"
+                  }
+                )
+              },
+              saveToPB: () => {
+                postMessage({
+                      "func": "envsave",
+                      "storage":"pastebuffer"
+                  }
+                )
+              },
+              loadGist: (gistid) => {
+                postMessage({
+                      "func": "envload",
+                      "name": gistid,
+                      "storage":"gist"
+                  }
+                )
+              },
 
-  send(value) {
-    if (this.ringbuf.available_write() > 1) {
-      if (typeof(value) == "number") {
-        this.ringbuf.push(new Float64Array([value]));
-      }else{
-        if (value.length == this.blocksize) {
-          this.ringbuf.push(value);
-        }else if (value.length < this.blocksize) {
-          let newVal = new Float64Array(this.blocksize);
-          for(let i in value) newVal[i] = value[i];
-          this.ringbuf.push(newVal);
-        }else{
-          this.ringbuf.push(value.slice(0,this.blocksize));
-        }
-      }
-    }
-  }
-
-}
-
-var createOutputChannel = (id, blocksize) => {
-  return new MLSABOutputTransducer('ML', id, blocksize);
-};
-`);
-
-			geval(`
-var loadResponders = {};
-var inputSABs={};
-var sema = {
-  saveF32Array: (name, val) => {
-    postMessage({
-      "func": "save",
-      "name": name,
-      "val": val
-    });
-    return 0;
-  },
-  loadF32Array: (name, onload) => {
-    postMessage({
-      "func": "load",
-      "name": name,
-    });
-    loadResponders[name] = onload;
-    return 0;
-  },
-  download: (name) => {
-    postMessage({
-      "func": "download",
-      "name": name,
-    });
-  },
-  sendCode: (code) => {
-    postMessage({
-      "func": "sendcode",
-      "code": code,
-    });
-  },
-  pbcopy: (msg) => {
-    postMessage({
-      "func": "pbcopy",
-      "msg": msg,
-    });
-  },
-  sendBuffer: (bufferName,data) => {
-      postMessage({
-          "func": "sendbuf",
-          "name": bufferName,
-          "data": data
-      });
-  },
-  env: {
-    saveLocal: (name) => {
-      postMessage({
-            "func": "envsave",
-            "name": name,
-            "storage":"local"
-        }
-      )
-    },
-    loadLocal: (name) => {
-      postMessage({
-            "func": "envload",
-            "name": name,
-            "storage":"local"
-        }
-      )
-    },
-    saveToPB: () => {
-      postMessage({
-            "func": "envsave",
-            "storage":"pastebuffer"
-        }
-      )
-    },
-    loadGist: (gistid) => {
-      postMessage({
-            "func": "envload",
-            "name": gistid,
-            "storage":"gist"
-        }
-      )
-    },
-
-  },
-  //run in the DOM
-  domeval: (code) => {
-    postMessage({
-          "func": "domeval",
-          "code": code,
-      }
-    )
-  },
-  peerinfo: () => {
-    postMessage ({
-      "func": "peerinfo"
-    });
-    console.log("Your peer ID has been copied to the paste buffer")
-  }
-};
-`);
-		}
-		try {
-
-			// let evalRes = geval("var channel0 = createOutputChannel(0, 1)");
-		} catch (err) {
-			console.error("ERROR:eval:", err);
-		}
+            },
+            //run in the DOM
+            domeval: (code) => {
+              postMessage({
+                    "func": "domeval",
+                    "code": code,
+                }
+              )
+            },
+            peerinfo: () => {
+              postMessage ({
+                "func": "peerinfo"
+              });
+              console.log("Your peer ID has been copied to the paste buffer")
+            }
+          };
+      `);
+	  } catch (err) {
+		  console.error("ERROR:eval:", err);
+	  }
 	}
 
   function initWithURL(url){
-    if(new URL(url)){
-
+    if( new URL(url) ){
       try {
         importScripts(url + "/lalolib.js");
       } catch (err) {
@@ -195,15 +185,20 @@ var sema = {
 
       try{
         gevalAll();
+      } catch (err) {
+        console.error("ERROR: gevalAll", err);
+      }
+
+      try{
         sabChecker();
       } catch (err) {
-        console.error("ERROR: on importScripts, gevalAll and sabChecker", err);
+        console.error("ERROR: sabChecker", err);
       }
 
       postMessage({ init: true });
     }
     else
-      console.error("ERROR:worker:initWithURL: Invalid URL");
+      console.error("ERROR: initWithURL – Invalid URL");
   }
 
 onmessage = m => {
