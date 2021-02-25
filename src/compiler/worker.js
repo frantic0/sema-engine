fa72148ac244fa89a2a43f60da870a17dcfe16fb
Francisco Bernardo <f.bernardo@sussex.ac.uk>
2021-02-25 16:45:35 +0000

    preparing compilation worker

--- src/compiler/worker.js
+++ src/compiler/worker.js
@@ -4,6 +4,22 @@ import semaa from './sema.js';
 // import cloneDeep from "lodash.cloneDeep";
 
 
+
+
+export function evalToGlobalScope(source) {
+	// let moo = mooo; //  `let` local scope, works with eval – does NOT work with Geval
+	self.moo = mooo; //  global scope, works with Geval
+	// let sema = semaa; // does NOT work with Geval, works with eval
+	self.sema = semaa; // global scope, works with Geval
+	sema.num("3"); // hack to force the module to load before eval
+	let module = { exports: "" };
+	// eval(source);
+	var geval = eval; // eval in the global scope, avoiding rollup warning - https://rollupjs.org/guide/en/#avoiding-eval
+	geval(source); // does NOT work with geval – ReferenceError: moo is not defined
+	// inject parser in window.grammar
+}
+
+
 function getParserModuleExports(source) {
   let mooo = moo;
   let sema = semaa;
@@ -16,30 +32,28 @@ function getParserModuleExports(source) {
 const clone = (a) => JSON.parse(JSON.stringify(a))
 
 
-onmessage = function(message) {
-  if (
-		message.data !== undefined &&
-		message.data.length != 0 &&
-    message.data.type === 'parse'
-		// message.data.type !== "webpackWarnings" &&  // [TODO:FB] This worker is being bombarded with global scope messages! Investigate to improve performance
-		// message.data.type !== "webpackClose"
-	) {
+onmessage = function(m) {
+  if ( m && m.data ){
 		try {
 			let parserOutputs = [];
-			const { liveCodeSource, parserSource } = message.data;
-			let parser = new nearley.Parser(getParserModuleExports(parserSource));
+			const { liveCodeSource, parserSource } = m.data;
+
+			// let parser = new nearley.Parser(getParserModuleExports(parserSource));
+
+			evalToGlobalScope(parserSource);
+      let parser = new nearley.Parser(self.grammar);
 
       parser.feed(liveCodeSource);
-			parserOutputs = JSON.parse(JSON.stringify(parser.results));
+
+			// parserOutputs = JSON.parse(JSON.stringify(parser.results));
 
       postMessage({
         output: parser.results,
 			});
 
     } catch (e) {
-			console.log("DEBUG:workerParser:onmessage:catch");
-			console.dir(e.message);
-      postMessage(e.message); // This sends parse errors caught with exception to the client for visibility! Do not remove!
+			console.error(e);
+			postMessage(e.message); // This sends parse errors caught with exception to the client for visibility! Do not remove!
 		}
 	}
 };
